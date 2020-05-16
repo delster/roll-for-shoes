a1870cedc1e0833815266eb3d07b2d6dc30300d1
David Elster <davidandtheweb@gmail.com>
2020-05-15 23:13:49 -0400

    Scaffolding (:seedling: styling)

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,7 +1,4 @@
-/**
- * Implement Gatsby's Browser APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/browser-apis/
- */
-
-// You can delete this file if you're not using it
+import 'typeface-montserrat'
+import 'tailwindcss/dist/base.css'
+import 'tailwindcss/dist/components.css'
+import 'tailwindcss/dist/utilities.css'
--- src/pages/page-2.tsx
+++ src/pages/page-2.tsx
@@ -1,5 +1,7 @@
 // Gatsby supports TypeScript natively!
 import React from "react"
+import styled from 'styled-components'
+import tw from 'tailwind.macro'
 import { PageProps, Link } from "gatsby"
 
 import Layout from "../components/layout"
@@ -11,7 +13,16 @@ const SecondPage = (props: PageProps) => (
     <h1>Hi from the second page</h1>
     <p>Welcome to page 2 ({props.path})</p>
     <Link to="/">Go back to the homepage</Link>
+    <Flex>
+      <p>hi</p>
+    </Flex>
   </Layout>
 )
+
+const Flex = styled.div`
+  ${tw`mx-auto py-10 text-center`};
+  background:red;
+
+`
 
 export default SecondPage
