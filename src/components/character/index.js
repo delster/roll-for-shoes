338d223f9246a6f1c9af2d1a74707730614cda3c
David Elster <davidandtheweb@gmail.com>
2020-05-16 23:39:53 -0400

    :construction: Character Scaffolding

--- src/components/character/index.js
+++ src/components/character/index.js
@@ -1,22 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
 import tw from 'tailwind.macro'
 
-const dummyCharacter = {
-  name: 'Blahblah Hoggins',
-  description: 'Yes, he is _that_ Blahblah.',
-  skills: ['skill 1', 'skill 2', 'skill 3']
-}
+export default ({ char }) => {
+  const [name, setName] = useState(char.name ?? '')
+  const [description, setDescription] = useState(char.description ?? '')
+  const [skills, setSkills] = useState(char.skills ?? [])
+  const handleNameChange = val => setName(val)
+  const handleDescriptionChange = val => setDescription(val)
+  const handleSkillsChange = val => setSkills(val)
 
-export default ({ char }) => (
-  <Character>
-    <Name>{char.name}</Name>
-    <Description>{char.description}</Description>
-    <Skills>
-      {char.skills.map(skill=><Skill key={skill} skill={skill} />)}
-    </Skills>
-  </Character>
-)
+  return (
+    <Character>
+      <Name>{name}</Name>
+      <Description>{description}</Description>
+      <Skills>
+        {skills.map(skill => (
+          <Skill key={skill} skill={skill} />
+        ))}
+      </Skills>
+    </Character>
+  )
+}
 
 const Character = styled.div`
   ${tw`mx-auto py-10 text-center`};
