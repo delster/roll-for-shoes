9e2f2fc6e4e2aedee9dece66e8e21081d36c17d0
David Elster <davidandtheweb@gmail.com>
2020-05-16 23:40:10 -0400

    :sparkles: Add Game to Page

---
+++ src/components/game/index.js
@@ -0,0 +1,46 @@
+import React, { useState } from 'react'
+import styled from 'styled-components'
+import tw from 'tailwind.macro'
+import { FaCheck, FaPencilAlt, FaTimes } from 'react-icons/fa'
+import CharacterSheet from '../character'
+
+export default () => {
+  const [id, setId] = useState(1)
+  const [title, setTitle] = useState('')
+  const [characters, setCharacters] = useState([])
+  const [isEditingTitle, setIsEditingTitle] = useState(false)
+
+  const toggleEditMode = () => setIsEditingTitle(!isEditingTitle)
+  const handleTitleEdit = e => setTitle(e.currentTarget.value)
+
+  return (
+    <Game>
+      <GameTitle>
+        Game #{id}: {isEditingTitle || title}
+        {isEditingTitle && (
+          <TitleInput value={title} onChange={handleTitleEdit} />
+        )}
+        <TitleToggle val={isEditingTitle} onToggle={toggleEditMode} />
+      </GameTitle>
+
+      <CharList>
+        {characters.map(c => (
+          <CharacterSheet key={c.name} char={c} />
+        ))}
+      </CharList>
+    </Game>
+  )
+}
+
+const Game = styled.div`
+  ${tw`m-8 p-4 font-mono bg-gray-100 shadow-md`}
+`
+const GameTitle = styled.h2`
+  & svg {
+    ${tw`inline-block ml-2`}
+  }`
+const CharList = styled.ul``
+const TitleInput = styled.input``
+
+const TitleToggle = ({ val, onToggle }) =>
+  val ? <FaCheck onClick={onToggle} /> : <FaPencilAlt onClick={onToggle} />
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,10 +2,12 @@ import React from 'react'
 import Layout from '@components/layout'
 import SEO from '@utils/seo'
 import Rules from '@components/rules'
+import Game from '@components/game'
 
 export default () => (
   <Layout>
     <SEO title="Roll for Shoes" />
     <Rules />
+    <Game />
   </Layout>
 )
